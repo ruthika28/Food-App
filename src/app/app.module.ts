59d957fa91b7ac598800c63486edef7efdbf9c12
keerthi-reddy <r.keerthireddy74@gmail.com>
2020-01-27 22:52:59 +0530

    articles page(only html)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideComponent } from './side/side.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { HealthyComponent } from './tags/healthy/healthy.component';
+import { ChickenComponent } from './tags/chicken/chicken.component';
+import { VegetarianComponent } from './tags/vegetarian/vegetarian.component';
+import { DesertsComponent } from './tags/deserts/deserts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideComponent
+    SideComponent,
+    ArticlesComponent,
+    HealthyComponent,
+    ChickenComponent,
+    VegetarianComponent,
+    DesertsComponent
   ],
   imports: [
     BrowserModule,
