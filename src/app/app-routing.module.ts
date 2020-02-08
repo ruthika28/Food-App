dd011e6df0a374899c346f4b5c7d39fc559a1f20
keerthi-reddy <r.keerthireddy74@gmail.com>
2020-02-08 12:43:04 +0530

    edited app,home,browse,articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,13 @@ import { HealthyComponent } from './tags/healthy/healthy.component';
 import { VegetarianComponent } from './tags/vegetarian/vegetarian.component';
 import { ChickenComponent } from './tags/chicken/chicken.component';
 import { DesertsComponent } from './tags/deserts/deserts.component';
+import { BrowseComponent } from './browse/browse.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [{path:'',redirectTo:'home',pathMatch:'full'},
+{path:'browse', component:BrowseComponent},
+{path:'home',component:HomeComponent},
 {path:'articles',component:ArticlesComponent,children:
 [{path:'tags/healthy',component:HealthyComponent},
 {path:'tags/vegetarian',component:VegetarianComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { SideComponent } from './side/side.component';
 import { HomeComponent } from './home/home.component';
-=======
 import { ArticlesComponent } from './articles/articles.component';
 import { HealthyComponent } from './tags/healthy/healthy.component';
 import { ChickenComponent } from './tags/chicken/chicken.component';
 import { VegetarianComponent } from './tags/vegetarian/vegetarian.component';
 import { DesertsComponent } from './tags/deserts/deserts.component';
->>>>>>> 56dba4e16436732dbe9dcb21dd7fe2273a51c88f
+import { BrowseComponent } from './browse/browse.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    SideComponent,
-    HomeComponent
-=======
+    HomeComponent,
     ArticlesComponent,
     HealthyComponent,
     ChickenComponent,
     VegetarianComponent,
-    DesertsComponent
->>>>>>> 56dba4e16436732dbe9dcb21dd7fe2273a51c88f
+    DesertsComponent,
+    BrowseComponent  
   ],
   imports: [
     BrowserModule,
